3efba98f76d077dcb0b141e1412403024c2f90d2
Arch-Noize <santiagonoize@gmail.com>
2023-06-30 05:52:50 -0400

    Add JS functions

--- script.js
+++ script.js
@@ -82,3 +82,93 @@ function program () {
 window.addEventListener('DOMContentLoaded', program);
 
 // Speaker generation
+
+const featured = [
+  {
+    id: '1',
+    show: '',
+    img: './Assets/speakers/speaker-1.png',
+    person: 'Yochai Benkier',
+    title: 'Professional Sound mixer and VFX',
+    desc: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+  },
+  {
+    id: '2',
+    show: '',
+    img: './Assets/speakers/speaker-2.png',
+    person: 'Kilnam',
+    title: '',
+    desc: 'Kilnam Chon helped bring the internet to Asia and is an outspoken advocate for the open web and digital com-mons. In 2012. he was inducted into the inaugural class of the Internet Society’s (ISOC) Internet Hall of Fame',
+  },
+  {
+    id: '3',
+    show: 'show',
+    img: './Assets/speakers/speaker-3.png',
+    person: 'SohYeong',
+    title: 'Director of Art Centre Nabi and a board member of CC Korea',
+    desc: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+  },
+  {
+    id: '4',
+    show: 'show',
+    img: './Assets/speakers/speaker-4.png',
+    person: 'Julia',
+    title: 'Director of Art Centre Nabi and a board member of CC Korea',
+    desc: 'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
+  },
+  {
+    id: '5',
+    show: 'show',
+    img: './Assets/speakers/speaker-5.png',
+    person: 'Lila',
+    title: 'President of Young Pirates of Europe',
+    desc: 'Lila Tretikov is the Executive of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languag-es and used by nearly half a billion people around the world every month.',
+  },
+  {
+    id: '6',
+    show: 'show',
+    img: './Assets/speakers/speaker-6.png',
+    person: 'Ryan',
+    title: 'CEO of Creativve Commons, ex COO of the Mozilla Foundation',
+    desc: 'Ryan had been leading open-source projects at the Mozilla Foundation such as the open-source move-ment',
+  },
+]
+
+const listSpeakers = document.querySelector("#speakers");
+
+function featSpeaker () {
+  let speakers = featured.map((item) =>
+  `<div class="featured-speaker ${item.show}">
+      <div class="container" id="speaker-image">
+        <img src="${item.img}">
+      </div>
+      <div class="container" id="speaker-detail">
+        <h3>${item.person}</h3>
+        <h5>${item.title}</h5>
+        <hr>
+        <p>${item.desc}</p>
+      </div> 
+    </div>
+  `  
+  );
+  speakers = speakers.join('');
+  listSpeakers.innerHTML = speakers;
+}
+
+window.addEventListener('DOMContentLoaded', featSpeaker);
+
+// Show More / Less Button
+
+const more = document.querySelector("#more");
+
+function showMore () {
+  const hidden = document.querySelectorAll('.show');
+
+  hidden.forEach((item) =>
+    item.classList.remove('show')
+  );
+
+  more.classList.add('show');
+}
+
+more.addEventListener('click', showMore);
